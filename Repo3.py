8e7c2ee9942cab0b10d5039cb664887d28e88aee
Joanna <jbmarrett@gmail.com>
2024-04-17 23:18:15 +0100

    modified

---
+++ Repo3.py
@@ -0,0 +1,34 @@
+'''
+Ask user to enter a number
+ask the user to if they want to stop
+convert to float if added a demcial number
+if user enters -1 then the programme will stop
+update num_loopy and count
+calculate and print average
+'''
+total = 0
+count = 0
+
+
+while True:
+    num_loopy = input("Enter a number or enter -1 to stop: ")
+    if num_loopy == "-1":
+         break
+    try:
+        num_entered = float(num_loopy)
+    
+    except ValueError:
+        print ("Invalid, please enster a valid number")
+
+total += num_entered
+count += 1
+  
+if count > -1:
+        print("Programme stopped")
+
+        average = total / count # calculate average
+
+        print(f"The average of entered numbers is: {average}") # print the average
+
+
+        
