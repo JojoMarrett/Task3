5f4f34b6bafbf49cc9cbef2ec8c1d9fd79250066
Joanna <jbmarrett@gmail.com>
2024-04-18 16:28:53 +0100

    readdressed the code and made changes

--- Repo3.py
+++ Repo3.py
@@ -19,17 +19,17 @@ while True:
     
     except ValueError:
         print ("Invalid, please enster a valid number")
-        continue # Skip increase when output is invalid
+
 total += num_entered
 count += 1
   
-if count > 0:
-    print("Programme stopped")
-    average = total / count # calculate average
-    print(f"The average of entered numbers is: {average}") # print the average
+if count > -1:
+        print("Programme stopped")
 
-else:
-    print("No valid numbers are entered. Cannot calculate an average.")
+        average = total / count # calculate average
+
+        print(f"The average of entered numbers is: {average}") # print the average
+
 
 
         
